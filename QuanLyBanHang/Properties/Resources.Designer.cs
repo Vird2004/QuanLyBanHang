7202be6e5282355447605d62872fd52801ffebd1
Phú Nguyễn <phu.nt2710@gmail.com>
2024-12-26 00:03:48 +0700

    Revert "dyagdadydydwydagdyaw"
    This reverts commit 2d38bcb6069178c4366a0883939a66bda31f65ac.

--- QuanLyBanHang/Properties/Resources.Designer.cs
+++ QuanLyBanHang/Properties/Resources.Designer.cs
@@ -223,16 +223,6 @@ namespace QuanLyBanHang.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap view {
-            get {
-                object obj = ResourceManager.GetObject("view", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap XXXXXXXXXXXX {
             get {
                 object obj = ResourceManager.GetObject("XXXXXXXXXXXX", resourceCulture);
