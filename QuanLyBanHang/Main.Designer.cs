7474b9eac091c85ad74f7943bd7bb5e431f07873
Quốc Vượng Nguyễn <reddragon.vird04@gmail.com>
2025-01-06 18:39:41 +0700

    ??

--- QuanLyBanHang/Main.Designer.cs
+++ QuanLyBanHang/Main.Designer.cs
@@ -133,24 +133,24 @@
             // 
             // BTN_QuanlyKH
             // 
-            this.BTN_bill.AutoRoundedCorners = true;
-            this.BTN_bill.BorderRadius = 31;
-            this.BTN_bill.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
-            this.BTN_bill.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
-            this.BTN_bill.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
-            this.BTN_bill.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
-            this.BTN_bill.FillColor = System.Drawing.Color.DarkSlateBlue;
-            this.BTN_bill.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BTN_bill.ForeColor = System.Drawing.Color.White;
-            this.BTN_bill.Image = global::QuanLyBanHang.Properties.Resources.receipt;
-            this.BTN_bill.ImageAlign = System.Windows.Forms.HorizontalAlignment.Right;
-            this.BTN_bill.Location = new System.Drawing.Point(11, 232);
-            this.BTN_bill.Margin = new System.Windows.Forms.Padding(4);
-            this.BTN_bill.Name = "BTN_bill";
-            this.BTN_bill.Size = new System.Drawing.Size(268, 65);
-            this.BTN_bill.TabIndex = 2;
-            this.BTN_bill.Text = "IN HÓA ĐƠN";
-            this.BTN_bill.Click += new System.EventHandler(this.BTN_bill_Click);
+            this.BTN_QuanlyKH.AutoRoundedCorners = true;
+            this.BTN_QuanlyKH.BorderRadius = 31;
+            this.BTN_QuanlyKH.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.BTN_QuanlyKH.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.BTN_QuanlyKH.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.BTN_QuanlyKH.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.BTN_QuanlyKH.FillColor = System.Drawing.Color.DarkSlateBlue;
+            this.BTN_QuanlyKH.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTN_QuanlyKH.ForeColor = System.Drawing.Color.White;
+            this.BTN_QuanlyKH.Image = global::QuanLyBanHang.Properties.Resources.receipt;
+            this.BTN_QuanlyKH.ImageAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.BTN_QuanlyKH.Location = new System.Drawing.Point(11, 232);
+            this.BTN_QuanlyKH.Margin = new System.Windows.Forms.Padding(4);
+            this.BTN_QuanlyKH.Name = "BTN_QuanlyKH";
+            this.BTN_QuanlyKH.Size = new System.Drawing.Size(268, 65);
+            this.BTN_QuanlyKH.TabIndex = 2;
+            this.BTN_QuanlyKH.Text = "Quản lý khách hàng";
+            this.BTN_QuanlyKH.Click += new System.EventHandler(this.BTN_bill_Click);
             // 
             // BTN_ADD
             // 
